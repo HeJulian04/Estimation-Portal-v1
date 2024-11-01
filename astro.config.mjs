cfe1744557dd7d781119a98dbcc5f0cb5f454c31
Julian Henz <julian.henz@youengineering.ch>
2024-11-01 18:01:01 +0100

    fix types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+export default defineConfig({
+    vite: { plugins: [basicSsl()] },
+    server: { port: 3000 },
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                blogPost: 'storyblok/BlogPost',
+                blogPostList: 'storyblok/BlogPostList',
+                page: 'storyblok/Page',
+            },
+            apiOptions: {
+                // Choose your Storyblok space region
+                region: 'eu', // optional,  or 'eu' (default)
+            },
+        })
+    ],
+});
