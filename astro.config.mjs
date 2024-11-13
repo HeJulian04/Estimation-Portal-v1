f4556317149eebb03d2ffbf1dacdcc559db181c0
Julian Henz <julian.henz@youengineering.ch>
2024-11-13 21:10:16 +0100

    accessToken manuel written

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
-
 export default defineConfig({
     vite: { plugins: [basicSsl()] },
     server: { port: 3000 },
     integrations: [
         storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
+            accessToken: 'rhYWSI0XBRYFtV58NX1LsQtt',
             components: {
                 blogPost: 'storyblok/BlogPost',
                 blogPostList: 'storyblok/BlogPostList',
